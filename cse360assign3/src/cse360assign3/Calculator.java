4af293fb07dd1f4da51d243b48e5e1d5f58f998d
Andres Avila <aavila17.asu.edu>
2016-02-22 12:55:12 -0700

    Initial Version

---
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -0,0 +1,47 @@
+/**
+ * Practice with test driven development and into to git and github
+ * @author Andres Avila
+ * @version Feb 22, 2016
+ */
+
+package cse360assign3;
+
+public class Calculator {
+
+	private int total;
+	
+	/**Calculator Constructor */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**returns total */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**adds the value */
+	public void add (int value) {
+		
+	}
+	
+	/**subtracts the value */
+	public void subtract (int value) {
+		
+	}
+	
+	/**multiplies the value */
+	public void multiply (int value) {
+		
+	}
+	
+	/**divides the value */
+	public void divide (int value) {
+		
+	}
+	
+	/**returns previous actions */
+	public String getHistory () {
+		return "";
+	}
+}
